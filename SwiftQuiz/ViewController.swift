c4a8d6e7f1be70ac3ebfefbc4cfe4cc0e1564b0b
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-04-27 23:06:12 +0300

    Initial Commit

---
+++ SwiftQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by Aleksandr Morozov on 27.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
