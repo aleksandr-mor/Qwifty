20f0a96e04d35a29d86da1734796892eb3b5a7f1
Aleksandr Morozov <dr.morozov@hotmail.com>
2022-05-24 00:31:22 +0300

    Animation

--- SwiftQuiz/Controllers/StartViewController.swift
+++ SwiftQuiz/Controllers/StartViewController.swift
@@ -11,11 +11,63 @@ class StartViewController: UIViewController {
     
     //MARK: - IBOutlets
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var appImage: UIImageView!
+    @IBOutlet weak var rightCatImage: UIImageView!
+    @IBOutlet weak var leftCatImage: UIImageView!
+    @IBOutlet weak var topCatImage: UIImageView!
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         playButton.addShadowView()
+        titleLabel.alpha = 0
+        subtitleLabel.alpha = 0
+        appImage.alpha = 0
+        leftCatImage.alpha = 0
+        rightCatImage.alpha = 0
+        topCatImage.alpha = 0
+    }
+    //MARK: - Animation
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.5, animations: {
+            self.appImage.alpha = 1
+        }) {(true) in
+            UIView.animate(withDuration: 0.5, animations: {
+                self.titleLabel.alpha = 1
+            }) {(true) in
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.subtitleLabel.alpha = 1
+                }) {(true) in
+                    UIView.animate(withDuration: 1, delay: 1, animations: {
+                        self.leftCatImage.alpha = 1
+                    }) {(true) in
+                        UIView.animate(withDuration: 1, delay: 3, animations: {
+                            self.leftCatImage.alpha = 0
+                        }) {(true) in
+                            UIView.animate(withDuration: 1, delay: 3, animations: {
+                                self.rightCatImage.alpha = 1
+                            }) {(true) in
+                                UIView.animate(withDuration: 1, delay: 3, animations: {
+                                    self.rightCatImage.alpha = 0
+                                }) {(true) in
+                                    UIView.animate(withDuration: 1, delay: 3, animations: {
+                                        self.topCatImage.alpha = 1
+                                    }) {(true) in
+                                        UIView.animate(withDuration: 1, delay: 3, animations: {
+                                            self.topCatImage.alpha = 0
+                                        }, completion: { (true) in
+                                        })
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     //MARK: - IBActions
